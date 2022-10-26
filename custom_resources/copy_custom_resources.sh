df00fd4b7d048797625738697624496eef356e0e
Umer <umer.altaf@unimelb.edu.au>
2022-10-26 11:31:33 +1100

    Custom Resource Script

--- custom_resources/copy_custom_resources.sh
+++ custom_resources/copy_custom_resources.sh
@@ -13,10 +13,10 @@ then
 else
   mkdir -p "${GEOSERVER_DATA_DIR}"/security/auth/aurin-geoserver-auth
 
-  cp /custom_resuorces/geoserver-config.xml "${GEOSERVER_DATA_DIR}"/security/auth/aurin-geoserver-auth/config.xml
-  cp /custom_resuorces/config.xml "${GEOSERVER_DATA_DIR}"/security/config.xml
-  cp /custom_resuorces/monitor.properties "${GEOSERVER_DATA_DIR}"/monitoring/monitor.properties
-  cp /custom_resuorces/content.ftl "${GEOSERVER_DATA_DIR}"/monitoring/content.ftl
+  cp /custom_resources/config/geoserver-config.xml "${GEOSERVER_DATA_DIR}"/security/auth/aurin-geoserver-auth/config.xml
+  cp /custom_resources/config/config.xml "${GEOSERVER_DATA_DIR}"/security/config.xml
+  cp /custom_resources/config/monitor.properties "${GEOSERVER_DATA_DIR}"/monitoring/monitor.properties
+  cp /custom_resources/config/content.ftl "${GEOSERVER_DATA_DIR}"/monitoring/content.ftl
 
 #  sed -i "s#AURIN_GEOSERVER_AUTHZ_SERVICE#${AURIN_GEOSERVER_AUTHZ_SERVICE}#g" "${GEOSERVER_DATA_DIR}"/security/auth/aurin-geoserver-auth/config.xml
 fi
